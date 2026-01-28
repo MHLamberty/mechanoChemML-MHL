78c447acf0f1a3aefd78f2feca3b68d3fea5486b
MHLamberty <141678105+MHLamberty@users.noreply.github.com>
2026-01-28 15:43:42 -0500

    Update stepwise_minimization_HGOConverged.py

--- VSIAdjointRotatorCuff/stepwise_minimization_HGOConverged.py
+++ VSIAdjointRotatorCuff/stepwise_minimization_HGOConverged.py
@@ -4,48 +4,97 @@ import os
 import sys
 
 """
-
+The goal of the stepwise_minimization_HGOConverged function is to perform a 
+stepwise minimization of an objective function using the SciPy optimization library.
+SciPy uses the HGO algorithm to perform the optimization. The function takes in an 
+initial guess for the objective function, a set of arguments, and various options 
+for the optimization method. The function iteratively eliminates basis functions 
+from the optimization problem based on a threshold value for the change in the 
+objective function. The function returns the final optimized objective function 
+value and the corresponding basis function indices.
 """
 
 def iter_cb(m):
   print ("results = ", m)
   
-def stepwise_minimization_HGOConverged(obj_f, x0, args_dict,F_threshold=1.0e16, method_options={}, grad_f=None, save_to_file=None):
+def stepwise_minimization_HGOConverged(obj_f, x0, args_dict,F_threshold=1.0e16, 
+                                       method_options={}, grad_f=None, save_to_file=None):
   ##
-
+  """
+  Docstring for stepwise_minimization_HGOConverged
+  
+  :param obj_f: Objective function (loss function) to be minimized
+  :param x0: Initial guess for the current active basis function coefficients
+  :param args_dict: Dictionary of additional arguments for the objective function
+      active terms, target vector index, number of theta, mesh name, tendon stamp,  
+      combination, loss factor
+  :param F_threshold: Threshold for change in objective function to eliminate basis 
+      functions
+  :param method_options: Options for the optimization method for SciPy's minimize function
+      (optional)
+  :param grad_f: Gradient of the objective function (optional). If not provided,
+      SciPy will approximate the gradient numerically. jac=grad_f in sp.minimize
+  :param save_to_file: Path to save intermediate results (optional).
+  """
   callback=None
   bounds=None
+  # current_activate_index: indices of the currently active basis functions
+  # list/array of basis-term indices that are currently active in the optimization
+  # this is the current constitutive model hypothesis being tested (subset of basis functions
+  # of the candidate library)
   current_activate_index=args_dict['activate_basis_index']
+  # target_index: index of the target vector
   target_index=args_dict['target_vector_index']
+  # num_theta: number of theta values. Total number of candidate terms (size of library)
   num_theta=args_dict['num_theta']
+  # num_base_orign: number of basis functions
   num_base_orign=len(current_activate_index)
+  # meshName: name of the mesh, used to load the mesh
   meshName = args_dict['meshName']
+  # tendonStamp: date stamp of the tendon, used to identify the specific sample
   tendonStamp = args_dict['tendonStamp']
+  # combination: combination of basis functions
   combination = args_dict['combination']
+  # lossFactor: factor to scale the loss function, used to adjust the weight of the loss function
   lossFactor = args_dict['lossFactor']
   
+  # frozen_index: indices you are not allowed to drop (always keep these terms)
   frozen_index=[]
+  # max_eliminate_step: maximum number of elimination allowed
   max_eliminate_step=num_base_orign-1
   if 'frozen_index' in args_dict.keys():
     frozen_index=args_dict['frozen_index']
   if 'max_eliminate_step' in args_dict.keys():
     max_eliminate_step=args_dict['max_eliminate_step']
   if 'method' in args_dict.keys():
+    # method: scipy optimization method (e.g., 'L-BFGS-B', 'TNC', etc.)
+    # L-BFGS-B is a quasi-Newton method that approximates the Broyden-Fletcher-Goldfarb-Shanno (BFGS)
+    # algorithm using a limited amount of computer memory. It is particularly well-suited for 
+    # large-scale optimization problems.
     method=args_dict['method']
   if 'bounds' in args_dict.keys():
+    # bounds: coefficient bounds per active parameter (needed for L-BFGS-B and TNC methods)
     bounds=args_dict['bounds']
     
   if 'disp' in method_options.keys():
     if method_options['disp']==True:
       callback=iter_cb
+  # gamma_matrix: matrix to store the coefficients of the basis functions at each step
+  # shape: (num_theta, max_eliminate_step+1)
   gamma_matrix=np.empty((num_theta,max_eliminate_step+1))
   gamma_matrix[:]=np.NaN
+  # the target coefficient is always 1
+  # typical in VSI to fix one coefficient to 1 to avoid trivial rescaling ambiguity
   gamma_matrix[target_index,:]=1
   
+  # if save_to_file is provided, open the file for appending results
   if save_to_file!=None:
     f=open(save_to_file,'ab')
   
-  loss=np.zeros(max_eliminate_step+1)  
+  # loss: array to store the loss function value at each step
+  loss=np.zeros(max_eliminate_step+1)
+
+  # Step 0: optimize using the full initial set of active basis functions
   res=sp.minimize(obj_f, x0,jac=grad_f, method=method, 
                   args=(tendonStamp, meshName, lossFactor, combination,
                         current_activate_index,target_index),
